0f49ac75a7d4fcd49b299598c10ee88cac5bc3b0
LEONARDO ALVES DA SILVA <leonardoe5id@gmail.com>
2026-01-21 23:10:26 -0300

    adicionado método que salva usuário.

--- src/main/java/com/ms/usuario/infrastructure/security/JwtUtil.java
+++ src/main/java/com/ms/usuario/infrastructure/security/JwtUtil.java
@@ -1,12 +1,12 @@
 package com.ms.usuario.infrastructure.security;
 
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
-import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
